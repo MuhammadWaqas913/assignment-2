4564ab907257db3749c2d4dec78babec21cd5442
MuhammadWaqas913 <waqasmemon5616@gmail.com>
2024-12-13 13:05:44 +0500

    Add files via upload

---
+++ question1.dart
@@ -0,0 +1,11 @@
+void main() {
+List<String> days = ["monday","tuesday","wednesday","thursday","friday","saturday","sunday"];
+while (days.isNotEmpty);{
+String removedDays = days.removeLast();
+print(removedDays);
+
+
+
+
+}
+}
