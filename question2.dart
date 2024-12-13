11fec82444dfcf6b012b59652a0a0e626cc13327
MuhammadWaqas913 <waqasmemon5616@gmail.com>
2024-12-13 13:06:28 +0500

    Add files via upload

---
+++ question2.dart
@@ -0,0 +1,20 @@
+void main() {
+List<String> days = [];
+days.add("monday");
+days.add("tuesday");
+days.add("wednesday");
+days.add("thursday");
+days.add("friday");
+days.add("saturday");
+days.add("sunday");
+print(days);
+
+
+
+
+
+
+
+
+
+}
